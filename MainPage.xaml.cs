b2fb4c5fd86690646d410fd4e06a3adaf90c2c84
Jürgen Jacobsen <jurgenjacobsen@outlook.com>
2025-03-01 14:28:36 +0000

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Bookshelf;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		new WebView
+		{
+			Source = "https://www.microsoft.com"
+		};
+	}
+}
+
